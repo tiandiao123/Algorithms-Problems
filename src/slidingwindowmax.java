5b03a8394c4ef088bdb917ae2a5e048b9feca072
Cuiqing Li <lixx3527@umn.edu>
2017-03-12 19:43:36 -0400

    add sliding window max problem

---
+++ src/slidingwindowmax.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class slidingwindowmax{
+
+    public static void main(String[] args){
+    	int[] nums=new int[]{1,2,5,3,8,9,10,11,3,4,-1};
+    	int[] res=maxSlidingWindow(nums,3);
+    	System.out.println("the final return res is:");
+    	for(int i=0;i<res.length;i++){
+            System.out.print(res[i]+" ");
+    	}
+    }
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums==null||nums.length<1) return new int[0];
+        Deque<Integer> deque=new ArrayDeque<>();
+        int[] res=new int[nums.length-k+1];
+        
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            while(!deque.isEmpty()&&deque.peek()<=i-k){
+                  deque.poll();
+            }
+            while(!deque.isEmpty()&&nums[deque.peekLast()]<=nums[i]){
+                deque.pollLast();
+            }
+            deque.offer(i);
+            if(i>=k-1){
+                res[j++]=nums[deque.peek()];
+            }
+        }
+        
+        return res;
+    }
+
+
+}
